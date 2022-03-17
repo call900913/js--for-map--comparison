1299a4cffc7db11a1c1b9f7220b4a6ea0680ebce
Ryan Christy <call900913@icloud.com>
2022-03-17 07:50:42 -0400

    Init repo

---
+++ README.txt
@@ -0,0 +1,5 @@
+# LISEZMOI
+
+This is a shallow note on JavaScript for loop and the array.map function.
+
+(I wrote this note to practice vim).
---
+++ note.js
@@ -0,0 +1,18 @@
+let array = [1, 2, 3]
+
+// Given an array, a for loop calls array[member] with each iteration. 
+
+for (let j = 0 ; j < some.length ; j+
+  // do something with array[j]
+}
+
+
+// Given an array, a JS Array.Map consumes a function and with it visits each element.
+// whatever the function returns, the .Map returns. non mutative. 
+array.Map( (member) => { return member**2  } ) -> [1, 4, 9]
+
+let newArray = array.Map( (member) => { return member**2  } )
+
+
+
+
